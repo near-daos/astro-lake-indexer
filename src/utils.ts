25f04848b24b55a502de2fc4c8d6bff6213a4eb7
andkom <andkom@users.noreply.github.com>
2022-06-27 22:36:15 +0300

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,13 @@ export const formatBlockHeight = (blockHeight: number) => {
   return String(blockHeight).padStart(12, '0');
 };
 
-export const matchAccounts = (string: string, accounts: string[]) => {
+export const matchAccounts = (
+  string: string | undefined | null,
+  accounts: string[],
+) => {
+  if (!string) {
+    return false;
+  }
   return accounts.some(
     (account) => string === account || string.includes(`.${account}`),
   );
